75aeefca7585c01ec9012d4f5357b28fca5b5431
Alif Siregar <alifsiregar21@gmail.com>
2021-08-07 23:29:45 +0700

    add navbar skeleton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import { GlobalStyles } from './styles';
 import { routes } from './routes';
 import Error from './pages/Error';
+import Navbar from './components/Navbar';
 
 function App(): JSX.Element {
   return (
     <>
       <GlobalStyles />
       <Router>
+        <Navbar />
         <Switch>
+          {/* General routes */}
           {routes.map((route) => (
             <Route
               exact={route.exact}
@@ -17,6 +24,7 @@ function App(): JSX.Element {
               component={route.component}
             />
           ))}
+          {/* Route for 404 error page */}
           <Route
             component={Error}
           />
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = (): JSX.Element => (
+  <div>
+    <h1>
+      Navbar
+    </h1>
+  </div>
+);
+
+export default index;
