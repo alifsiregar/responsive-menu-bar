e0b7d54a66cbf7f6a041baf7004bdeccd40f1a9a
Alif Siregar <alifsiregar21@gmail.com>
2021-08-07 22:46:40 +0700

    implement routing in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { GlobalStyles } from './styles';
+import { routes } from './routes';
+import Error from './pages/Error';
 
 function App(): JSX.Element {
   return (
     <>
       <GlobalStyles />
-      <h3>
-        Project Skelton
-      </h3>
+      <Router>
+        <Switch>
+          {routes.map((route) => (
+            <Route
+              exact={route.exact}
+              path={route.path}
+              component={route.component}
+            />
+          ))}
+          <Route
+            component={Error}
+          />
+        </Switch>
+      </Router>
     </>
   );
 }
