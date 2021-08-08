6b0f0eee5e11768a5eacc386774eb008ca994bc7
Alif Siregar <alifsiregar21@gmail.com>
2021-08-08 10:20:08 +0700

    finish navbar component and styling

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Nav,
+  NavLogo,
+  MenuBar,
+  DesktopMenu,
+  MenuItems,
+  MobileNav,
+  MobileMenuToggle,
+  MenuClose,
+  MobileMenuItems,
+} from './styles';
+import { NavbarItems } from './NavbarItems';
+import aeLogo from '../../assets/images/ae-logo.png';
 
-const index = (): JSX.Element => (
-  <div>
-    <h1>
-      Navbar
-    </h1>
-  </div>
-);
+const Navbar = (): JSX.Element => {
+  const [showNav, setShowNav] = useState<boolean>(false);
 
-export default index;
+  const toggleMenu = ():void => {
+    setShowNav(!showNav);
+  };
+
+  return (
+    <>
+      <Nav>
+        <NavLogo src={aeLogo} />
+        <MenuBar onClick={toggleMenu} />
+        <DesktopMenu>
+          { NavbarItems.map((item):JSX.Element => (
+            <MenuItems exact={item.exact} to={item.path}>
+              {item.name}
+            </MenuItems>
+          )) }
+        </DesktopMenu>
+      </Nav>
+      <MobileNav isActive={showNav}>
+        <MobileMenuToggle>
+          <MenuClose onClick={toggleMenu} />
+        </MobileMenuToggle>
+        { NavbarItems.map((item):JSX.Element => (
+          <MobileMenuItems onClick={toggleMenu} exact={item.exact} to={item.path}>
+            {item.name}
+          </MobileMenuItems>
+        )) }
+      </MobileNav>
+    </>
+  );
+};
+export default Navbar;
---
+++ src/components/Navbar/styles.ts
@@ -0,0 +1,105 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+
+export const Nav = styled.div`
+    background-color: #18191A;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 2rem 0 2rem;
+`;
+
+export const NavLogo = styled.img`
+    width: 69px;
+    height: 69px;
+`;
+
+export const MenuBar = styled(FaIcons.FaBars)`
+    font-size: 2rem;
+    background: none;
+    color: #ffffff;
+
+    @media only screen and (min-width: 660px) {
+        display: none;
+    }
+`;
+
+export const DesktopMenu = styled.div`
+    width: 30rem;
+    margin-left: 2rem;
+    color: #ffffff;
+    display: flex;
+    justify-content: space-between;
+    display: none;
+
+    @media only screen and (min-width: 660px) {
+        display: flex;
+    }
+`;
+
+export const MenuItems = styled(NavLink)`
+    padding: 0.75rem;
+    border-radius: 0.25rem;
+    color: #ffffff;
+    text-decoration: none;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+        background-color: #1a83ff;
+    }
+
+    &.active {
+        background-color: #1a83ff;
+    }
+`;
+
+export const MobileNav = styled.div<{isActive:boolean}>`
+    background-color: #18191A;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: ${({ isActive }) => (isActive ? '0%' : '-200%')};
+    transition: 750ms;
+`;
+
+export const MobileMenuToggle = styled.div`
+    background-color: #18191A;
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+export const MenuClose = styled(AiIcons.AiOutlineClose)`
+    margin-right: 2rem;
+    font-size: 2rem;
+    background: none;
+    color: #ffffff;
+`;
+
+export const MobileMenuItems = styled(NavLink)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 8px 16px 8px 16px;
+    list-style: none;
+    height: 45px;
+    text-decoration: none;
+    color: #ffffff;
+    border-radius: 0.25rem;
+
+    &:hover {
+        background-color: #1a83ff;
+    }
+
+    &.active {
+        background-color: #1a83ff;
+    }
+`;
