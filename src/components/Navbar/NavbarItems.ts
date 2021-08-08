bc0fec035ae93086ea9a36654f6f0625efe58c46
Alif Siregar <alifsiregar21@gmail.com>
2021-08-08 10:19:31 +0700

    add items array for navbar items

---
+++ src/components/Navbar/NavbarItems.ts
@@ -0,0 +1,17 @@
+export const NavbarItems = [
+  {
+    name: 'Home',
+    path: '/',
+    exact: true,
+  },
+  {
+    name: 'About',
+    path: '/about',
+    exact: false,
+  },
+  {
+    name: 'Products',
+    path: '/products',
+    exact: false,
+  },
+];
