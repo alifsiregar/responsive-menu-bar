1aa005949f9f86c20a922677dddb9d84f6b1a9c4
Alif Siregar <alifsiregar21@gmail.com>
2021-08-09 16:43:58 +0700

    fix globalstyles bug with @import

--- src/styles.ts
+++ src/styles.ts
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
     * {
-        @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
         font-family: 'Lato', sans-serif;
         margin: 0;
         padding: 0;
